f0bcb461f9c6d3ae284a7fd3937040760d8728a3
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-01-06 19:21:46 -0300

    Port update to 8080, to Front End connection

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use('/api', blogRoutes); // Routes beeing used on the app
 
 app.use(function (error, req, res, next) {
