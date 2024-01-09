13e2981d435a97aa1ac7d3838ac4dcd8c23121a9
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-01-08 21:22:24 -0300

    Cors test check

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.use('/api', blogRoutes); // Routes beeing used on the app
 
