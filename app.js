f9a07418fbbd9fd02b778e0ac06bc65df1a1b778
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-01-10 14:11:25 -0300

    Cors Policy Error fix test

--- app.js
+++ app.js
@@ -13,9 +13,10 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Request-Width, Content-Type');
   next();
 });
 
